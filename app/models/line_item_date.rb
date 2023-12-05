1ae76430bae479174c19efb43d5e79a5ca166a8d
Kaleshe Alleyne-Vassel <kaleshe.alleyne-vassel@krystal.uk>
2023-12-05 11:26:37 +0000

    wip: ch10

--- app/models/line_item_date.rb
+++ app/models/line_item_date.rb
@@ -1,6 +1,4 @@
 class LineItemDate < ApplicationRecord
-  has_many :line_item_dates, dependent: :destroy
-
   belongs_to :quote
 
   validates :date, presence: true, uniqueness: { scope: :quote_id }
